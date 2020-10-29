9ffa002c659ef8572c345bc5fabf7c2dab18a86f
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-29 14:53:09 +0300

    created the necessary templates

---
+++ build/resources/main/db/createtables.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE wildlife;
+
+\c wildlife
+
+CREATE TABLE sightings(
+id SERIAL PRIMARY KEY,
+speciesName VARCHAR,
+speciesAge INTEGER,
+speciesHealth VARCHAR,
+rangerId INTEGER,
+locationId INTEGER
+);
+
+CREATE TABLE rangers(
+id SERIAL PRIMARY KEY,
+rangerName VARCHAR NOT NULL,
+rangerBadgeNumber INTEGER NOT NULL,
+rangerContact VARCHAR NOT NULL
+);
+
+CREATE TABLE locations(
+id SERIAL PRIMARY KEY,
+name VARCHAR
+);
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 public class App {
     public static void main(String[] args) {
+        System.out.println("I will got to Germany");
     }
 }
